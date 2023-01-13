fac38d86e0f685402179e99791e99780c5c613dd
TMaksyutov <dadojkeee@mail.ru>
2023-01-13 15:37:22 +0300

    added helpers

---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,32 @@
+package helpers;
+
+import config.MobileConfig;
+import org.aeonbits.owner.ConfigFactory;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+
+public class Browserstack {
+    static MobileConfig config = ConfigFactory.create(MobileConfig.class, System.getProperties());
+
+    public static String
+            browserstackLogin = config.browserstackLogin(),
+            browserstackPassword = config.browserstackPassword(),
+            browserstackUrl = config.browserstackUrl(),
+            app = config.app();
+
+    public static String videoUrl(String sessionId) {
+        String url = format("https://api-cloud.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        return given()
+                .auth().basic(browserstackLogin, browserstackPassword)
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .path("automation_session.video_url");
+    }
+}
