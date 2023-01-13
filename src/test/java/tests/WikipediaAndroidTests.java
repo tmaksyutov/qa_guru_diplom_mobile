f45785940e27dea5c682e93b5bab3332e8f6814f
TMaksyutov <dadojkeee@mail.ru>
2023-01-13 16:38:34 +0300

    fix test

--- src/test/java/tests/WikipediaAndroidTests.java
+++ src/test/java/tests/WikipediaAndroidTests.java
@@ -38,6 +38,7 @@ public class WikipediaAndroidTests extends TestBase {
         });
         step("Verify page is correct", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+            $(AppiumBy.className("android.webkit.WebView")).click();
             $(AppiumBy.className("android.view.View")).shouldHave(text("Russia"));
         });
     }
